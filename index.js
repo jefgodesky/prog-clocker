66f504a155b4a6c7808c43acfdf68da93b648640
Jason Godesky <jefgodesky@gmail.com>
2021-09-25 13:26:38 -0400

    Ready to clock

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { Client, Intents } from 'discord.js'
 import config from './config/index.js'
 
-console.log(config.token)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once('ready', () => {
+  console.log('Prog Clocker is ready to clock')
+})
+
+client.login(config.token)
