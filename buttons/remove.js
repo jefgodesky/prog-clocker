3539a1d3d9f4883494301fac74cd34549d7a2e5b
Jason Godesky <jefgodesky@gmail.com>
2021-09-26 16:18:20 -0400

    Add new button: Remove

---
+++ buttons/remove.js
@@ -0,0 +1,12 @@
+import { getOptions, findClockById, reshowClock } from '../utils.js'
+
+const execute = async (id, state, interaction) => {
+  const { guild, uid } = getOptions(['guild', 'uid'], interaction)
+  const clock = findClockById(parseInt(id), guild, uid, state)
+  if (!clock) await interaction.reply({ content: 'Not sure what went wrong there. Maybe try again?', ephemeral: true })
+  clock.curr--
+  await reshowClock(interaction, clock)
+}
+
+const button = { id: 'remove', execute }
+export default button
